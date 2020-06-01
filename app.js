c007b4cce1935567b0f68ccf19ae0ebba696a661
dreplica <adejo.david@aiesec.net>
2020-06-01 22:19:56 +0100

    implementing routes for buy and sell

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./src/routes/index');
-var usersRouter = require('./src/routes/users');
+var buyRouter = require('./src/routes/buy');
+var sellRouter = require('./src/routes/sell');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +17,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/buy', buyRouter);
+app.use('/sell', sellRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
