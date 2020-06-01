4523568b8f9c95571a76f871c676b8e060efc7ec
dreplica <adejo.david@aiesec.net>
2020-06-01 22:04:00 +0100

    created mongoose schema and model, also created event listeners to watch out for mongoose connections

--- src/model/mongooseModel.js
+++ src/model/mongooseModel.js
@@ -1,1 +1,13 @@
 const mongoose = require('mongoose')
+
+const iphoneSchema = new mongoose.Schema({
+    phone: String,
+    spec: [{
+        memory: String,
+        price: Number,
+        quantity:Number,default:0
+    }]
+})
+
+const buyModel = mongoose.model('Buy',iphoneSchema)
+const sellModel = mongoose.model('Sell',iphoneSchema)
