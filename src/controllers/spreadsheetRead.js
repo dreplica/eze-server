02bb568496c8114606572eb33d2f7ac2645dc29a
dreplica <adejo.david@aiesec.net>
2020-06-02 19:55:30 +0100

    arranged spreadsheet from api, just for buyProducts

--- src/controllers/spreadsheetRead.js
+++ src/controllers/spreadsheetRead.js
@@ -1,4 +1,5 @@
-const { google } = require('googleapis')
+const { google } = require('googleapis');
+const { buyModel } = require('../model/mongooseModel');
 
 async function buyProductsData(auth) {
     return new Promise((resolve, reject) => {
@@ -12,21 +13,33 @@ async function buyProductsData(auth) {
                 if (err) return reject('The API returned an error: ' + err);
                 const rows = res.data.values;
                 if (rows.length) {
-                    console.log('buying, Major:');
-                    // Print columns A2 to J, which correspond to indices 0 and 4.
-                    rows.map((row) => {
-                        const model = {
-                            phone: "", spec: [{
-                                memory: "",
-                                price: 0,
-                                quantity: 0
-                            }]
-                        };
-                        // console.log(row);
-                        if (row.length === 1 && /iphone\./i.test(row)) {
-                        console.log("start "+row[0]+" end");
+                    const data = rows.reduce((acc,row,ind,arr) => {
+
+                        const phone = getPhones(row)
+                        if (phone) {
+                            if (phone && acc.count >= 0) {
+                                acc.item.push(acc.model);
+                                acc.model = {}
+                            }
+                            acc.model.phone = phone;
+                            acc.model.spec = []
+                            acc.count += 1;
                         }
-                    });
+                        if (acc.model.phone) {
+                            if (row.find(x => /\$/ig.test(x))) {
+                                acc.model.spec.push(distributeVaues(row))
+                            }
+                        }
+
+                        if (arr.length - 1 === ind) {
+                            acc.item.push(acc.model);
+                        }
+
+                        return acc;
+                    },{item:[],model:{},count:-1});
+
+                    console.log(data.item)
+                    return resolve(data.item)
                 } else {
                     console.log('No data found.');
                 }
@@ -35,36 +48,41 @@ async function buyProductsData(auth) {
     });
 }
 
-async function sellProductsData(auth) {
-    return new Promise((resolve, reject) => {
-        const sheets = google.sheets({ version: 'v4', auth });
-        sheets.spreadsheets.values.get(
-            {
-                spreadsheetId: '1F6BvjBRKMf6cVTzrb3O-4uORjnhHN0I6DC9jkuxQibo',
-                range: 'IPHONES!L2:U'
-            },
-            (err, res) => {
-                if (err) return reject('The API returned an error: ' + err);
-                const rows = res.data.values;
-                if (rows.length) {
-                    console.log('selling, Major:');
-                    // Print columns A2 to J, which correspond to indices 0 and 4.
-                    rows.map((row, i) => {
-                        const model = {};
-                        // if (row[i].length === 1 && /iphone/gi.test(row[i])) {
-                        // model.phone = row[i];
-                        // }
-                        console.log(row);
-                    });
-                } else {
-                    console.log('No data found.');
-                }
-            }
-        );
-    });
-}
+const sellProductsData = ()=>{}
 
 module.exports = {
     buyProductsData,
     sellProductsData
+}
+
+
+const getPhones = (arr) => {
+    if (arr.find(x => /iphone./ig.test(x))) {
+        return arr[0]
+    }
+}
+
+const distributeVaues = (arr = []) => {
+    const itemStructure = {
+        locked: true,
+        memory: 0,
+        price: []
+    }
+
+    arr.forEach((item, ind) => {
+        switch (ind) {
+            case 0:
+                if (/unlocked|\S/i.test(item)) itemStructure.locked = false;
+                break;
+
+            case 1:
+                itemStructure.memory = item;
+                break;
+
+            default:
+                itemStructure.price.push(item)
+                break;
+        }
+    })
+    return itemStructure
 }
