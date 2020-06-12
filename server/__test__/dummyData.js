510929cf11b80d9c8ea2e74ff98cb6c4d78c264c
dreplica <adejo.david@aiesec.net>
2020-06-12 05:38:43 +0100

    test implemented to check code

--- server/__test__/dummyData.js
+++
@@ -1,9 +0,0 @@
-export const insertDb = ({
-    condition: "A1",
-    image: "",
-    locked: "false",
-    memory: 256,
-    phone: "iphone 5s",
-    price: 900,
-    sell: "buy"
-});
--- server/__test__/index.test.js
+++ server/__test__/index.test.js
@@ -5,7 +5,7 @@ import 'regenerator-runtime/runtime'
 import app from '../app'
 import mongoServer from './mongosetup/memoryserver';
 import { iphoneSchema } from '../src/model/mongooseModel';
-import { insertDb } from './dummyData';
+import insertDb from './dummyData';
 
 const router = supertest(app)
 
@@ -28,96 +28,179 @@ describe('test for mongodb db', () => {
         const phone = mongoose.model('phone', iphoneSchema);
         const count = await phone.estimatedDocumentCount();
 
-        await phone.create({ ...insertDb })
-
-        const newcount = await phone.estimatedDocumentCount();
+        await phone.insertMany(insertDb)
 
         const getImage = await phone.find()
-        const phoneDBname = getImage[0]['phone'];
-        const phoneName = insertDb.phone
+        const testImageDBname = getImage[0]['phone'];
 
         expect(count).toEqual(0);
-        expect(phoneDBname).toEqual(phoneName);
-        expect(newcount).toEqual(1);
+        expect(testImageDBname).toMatch(/\w*_*/);
+        expect(getImage.length).toEqual(insertDb.length);
     });
 })
 
 describe('test for API endpoints', () => {
 
-    // test('test endpoints /api/ to return 200', async (done) => {
-    
-    //     router.get('/api/')
-    //         .expect('Content-type', /json/)
-    //         .expect(200, done)
-
-    //     router.get('/api/?limit=6&page=1&size=64&phone=iphone&sort=1&condition=a1&sell=buy')
-    //         .expect(200)
-    //         .end((err, res) => {
-    //             if (err) return done(err)
-    //             console.log("api/  dmamit", res.body)
-    //             const length = res.body.result.length
-    //             const condition = res.body.result.every((item) => item.condition === 'A1')
-    //             console.log("the length", length)
-    //             console.log("the condition", condition)
-    //             expect(length <= 6).toBe(true)
-    //             expect(condition).toBe(true)
-    //             done()
-    //         })
-    // })
-
-
-    // test('test endpoints /api/update to return 200', async (done) => {
-    //     router.get('/api/update')
-    //         .expect(200, done)
-    // })
-
-    test('test endpoints /api/buy to return 200', async (done) => {
-
-        // router.get('/api/buy')
-        //     .query({ sell: "buy" })
-        //     .expect('Content-type', /json/)
-        //     .expect(200, done)
+    test('test endpoints /api/ to return 200 And 404', async (done) => {
 
         router.get('/api/')
-            // .query({ phone: 'se', sell: "buy" })
-            .set('Accept', 'application/json')
-            .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200)
-            .then( res => {
-                // if (err) return done(err)
-                const length =  res.body.result.length
-                const condition =  res.body.result.every((item) => item.condition === 'A1')
-                // console.log("the length", length)
-                // console.log("the condition", condition)
+            .expect('Content-type', /json/)
+            .expect(200, done)
+    })
 
-                console.log(res.text)
-                expect(length <= 6).toBe(true)
+    test('test endpoints /api/ with query to return 200 ', async (done) => {
+        router.get('/api/?limit=6&page=1&phone=iphone&sort=1&condition=a1')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err
+
+                const length = res.body.result.length
+                const condition = res.body.result.every((item) => item.condition === 'A1')
+
+                expect(length >= 1 && length <= 6).toBe(true)
                 expect(condition).toBe(true)
                 done()
             })
     })
 
+    test('test endpoints /api/ with bad condition query', async (done) => {
+        router.get('/api/?limit=6&page=1&phone=iphone&sort=1&condition=a5')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err
+                const length = res.body.result.length
+                expect(length).toEqual(0)
+                done()
+            })
+    })
 
 
-    // test('test endpoints /api/sell to return 200', async (done) => {
+    test('test endpoints /api/update to return 200', async (done) => {
+        router.get('/api/update')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end((err, res) => {
 
-    //     router.get('/api/sell')
-    //         .expect('Content-type', /json/)
-    //         .expect(200, done)
+                if (err) {
+                    console.log('error :>> ', err);
+                    throw err
+                }
+                done()
+            })
 
-    //     router.get('/api/sell?limit=6&page=1&size=64&phone=iphone&sort=1&condition=a1&sell=sell')
-    //         .expect(200)
-    //         .end((err, res) => {
-    //             if (err) return done(err)
-    //             const length = res.body.result.length
-    //             const condition = res.body.result.every((item) => item.condition === 'A1')
-    //             console.log("the length", length)
-    //             console.log("the condition", condition)
-    //             expect(length <= 6).toBe(true)
-    //             expect(condition).toBe(true)
-    //             done()
-    //         })
-    // })
+    })
+
+    test('test endpoints for buy request to return 200 ', async (done) => {
+
+        router.get('/api?sell=buy')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err
+
+                const length = res.body.result.length
+                const condition = res.body.result.every((item) => item.sell === 'buy')
+
+                expect(length === 10).toBe(true)
+                expect(condition).toBe(true)
+                done()
+            })
+    })
+
+    test('test endpoints /api/ for "buy" request return value', async (done) => {
+
+        router.get('/api?sell=buy')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err
+
+                const length = res.body.result.length
+                const condition = res.body.result.some((item) => item.sell === 'sell')
+
+                expect(length > 10).toBe(false)
+                expect(condition).toBe(false)
+                done()
+            })
+    })
+
+    test('test endpoints /api/ for bad "buy" request', async (done) => {
+        router.get('/api?sell=buy&condition=a5')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err
+                const length = res.body.result.length
+                expect(length).toEqual(0)
+                done()
+            })
+    })
+
+    test('test endpoints for sell request to return 200 ', async (done) => {
+
+        router.get('/api?sell=sell')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err
+
+                const length = res.body.result.length
+                const condition = res.body.result.every((item) => item.sell === 'sell')
+
+                expect(length === 10).toBe(true)
+                expect(condition).toBe(true)
+                done()
+            })
+    })
+
+    test('test endpoints /api/ for "sell" request return value', async (done) => {
+
+        router.get('/api?sell=sell')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err
+
+                const length = res.body.result.length
+                const condition = res.body.result.some((item) => item.sell === 'buy')
+
+                expect(length > 10).toBe(false)
+                expect(condition).toBe(false)
+                done()
+            })
+    })
+
+    test('test endpoints /api/ for bad "sell" request', async (done) => {
+        router.get('/api?sell=sell&condition=a5')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err
+                const length = res.body.result.length
+                expect(length).toEqual(0)
+                done()
+            })
+    })
+    test('test endpoints /api/ for pagination', async (done) => {
+        router.get('/api')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err
+                const keys = Object.keys(res.body)
+                console.log('keys :>> ', keys);
+                expect(keys.length).toEqual(3)
+                expect(keys.includes('previous')).toBe(true)
+                expect(keys.includes('forward')).toBe(true)
+                expect(typeof res.body[keys[0]]).toBe('object')
+                expect(typeof res.body[keys[1]]).toBe('object') 
+                expect(res.body['previous']['page']).toBe(undefined) 
+                expect(res.body['forward']['page']).toBe(2) 
+                done()
+            })
+    })
 
 
 })
